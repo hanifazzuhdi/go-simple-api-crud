39138786a4582a079c9a3b15b6f304646bb10ad4
hanifazzuhdi <hnfhanif52@gmail.com>
2025-11-06 05:25:37 +0700

    Refactor CORS support

--- internal/app/router.go
+++ internal/app/router.go
@@ -12,26 +12,40 @@ func NewRouter(nationalityHandler *handler.NationalityHandler, customerHandler *
 
 	api := router.PathPrefix("/api").Subrouter()
 
-	api.HandleFunc("/nationalities", nationalityHandler.GetAll).Methods(http.MethodGet)
-	api.HandleFunc("/nationalities/{id}", nationalityHandler.GetById).Methods(http.MethodGet)
-	api.HandleFunc("/nationalities", nationalityHandler.Create).Methods(http.MethodPost)
-	api.HandleFunc("/nationalities/{id}", nationalityHandler.Update).Methods(http.MethodPut)
-	api.HandleFunc("/nationalities/{id}", nationalityHandler.Delete).Methods(http.MethodDelete)
+	api.HandleFunc("/nationalities", nationalityHandler.GetAll).Methods(http.MethodGet, http.MethodOptions)
+	api.HandleFunc("/nationalities/{id}", nationalityHandler.GetById).Methods(http.MethodGet, http.MethodOptions)
+	api.HandleFunc("/nationalities", nationalityHandler.Create).Methods(http.MethodPost, http.MethodOptions)
+	api.HandleFunc("/nationalities/{id}", nationalityHandler.Update).Methods(http.MethodPut, http.MethodOptions)
+	api.HandleFunc("/nationalities/{id}", nationalityHandler.Delete).Methods(http.MethodDelete, http.MethodOptions)
 
-	api.HandleFunc("/customers", customerHandler.GetAll).Methods(http.MethodGet)
-	api.HandleFunc("/customers/{id}", customerHandler.GetById).Methods(http.MethodGet)
-	api.HandleFunc("/customers", customerHandler.Create).Methods(http.MethodPost)
-	api.HandleFunc("/customers/{id}/families/sync", customerHandler.SyncCustomerFamilies).Methods(http.MethodPost)
-	api.HandleFunc("/customers/{id}", customerHandler.Update).Methods(http.MethodPut)
-	api.HandleFunc("/customers/{id}", customerHandler.Delete).Methods(http.MethodDelete)
+	api.HandleFunc("/customers", customerHandler.GetAll).Methods(http.MethodGet, http.MethodOptions)
+	api.HandleFunc("/customers/{id}", customerHandler.GetById).Methods(http.MethodGet, http.MethodOptions)
+	api.HandleFunc("/customers", customerHandler.Create).Methods(http.MethodPost, http.MethodOptions)
+	api.HandleFunc("/customers/{id}/families/sync", customerHandler.SyncCustomerFamilies).Methods(http.MethodPost, http.MethodOptions)
+	api.HandleFunc("/customers/{id}", customerHandler.Update).Methods(http.MethodPut, http.MethodOptions)
+	api.HandleFunc("/customers/{id}", customerHandler.Delete).Methods(http.MethodDelete, http.MethodOptions)
 
-	api.HandleFunc("/families", familyListHandler.GetAll).Methods(http.MethodGet)
-	api.HandleFunc("/families/{id}", familyListHandler.GetById).Methods(http.MethodGet)
-	api.HandleFunc("/families", familyListHandler.Create).Methods(http.MethodPost)
-	api.HandleFunc("/families/{id}", familyListHandler.Update).Methods(http.MethodPut)
-	api.HandleFunc("/families/{id}", familyListHandler.Delete).Methods(http.MethodDelete)
+	api.HandleFunc("/families", familyListHandler.GetAll).Methods(http.MethodGet, http.MethodOptions)
+	api.HandleFunc("/families/{id}", familyListHandler.GetById).Methods(http.MethodGet, http.MethodOptions)
+	api.HandleFunc("/families", familyListHandler.Create).Methods(http.MethodPost, http.MethodOptions)
+	api.HandleFunc("/families/{id}", familyListHandler.Update).Methods(http.MethodPut, http.MethodOptions)
+	api.HandleFunc("/families/{id}", familyListHandler.Delete).Methods(http.MethodDelete, http.MethodOptions)
 
-	router.Use(mux.CORSMethodMiddleware(router))
+	router.Use(handleCorsMiddleware)
 
 	return router
+}
+
+func handleCorsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, x-requested-with")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
